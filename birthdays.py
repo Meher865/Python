f76bd4ef6c1b6ee19b9f6317b8131b6030585a81
Meher865 <jasmeetsw@gmail.com>
2025-04-23 13:00:17 +0530

    Add files via upload

---
+++ birthdays.py
@@ -0,0 +1,10 @@
+mom= "my mom's birthday is on 2nd July,1978"
+dad= "my dad's birthday is on 3rd July, 1975"
+mine= "my birthday is on 15th May, 2013"
+sister= "my sister's birthday is on 31st October,2016"
+friend = "my friend's birthday is on the 12th September,2013"
+print(mom)
+print(dad)
+print(mine)
+print(sister)
+print(friend)
