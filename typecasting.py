ad72ee0a98de617ea8fb908862e16ca7ac22b1d3
Meher865 <jasmeetsw@gmail.com>
2025-04-24 12:39:37 +0530

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,16 @@
+a=156
+print("type of a :",a, type(a))
+b=34.90
+print("type of b:",type(b))
+c="pencils"
+print("type of c:",type(c))
+d=True
+print("type of d:",type(d))
+a=float(a)
+print("type of a :",type(a))
+b=int(b)    
+print("type of b:",type (b))
+c=bool(c)
+print("type of c:",type(c))
+d=str(d)
+print("type of d:",type(d))
