d373eba3cb6f1d3424e53f1dea41857d8184d4b0
Meher865 <jasmeetsw@gmail.com>
2025-04-29 15:35:20 +0530

    Add files via upload

---
+++ and or.py
@@ -0,0 +1,20 @@
+a=23
+b= 4567
+c=0
+if a and b and c:
+  print("all the numbers have boolean value as true")
+else:
+  print("at least one of the numbers has a boolean value false.")
+a=34
+b=0
+c=-56765
+if a > c or b > c:
+  print ("either of the numbers is greater than 0")
+else:
+  print(" neither of the numbers is greater than 0")
+if a > 0 or b > 0 or c > 0:
+  print ("eithe rone of these values is grater than 0")
+else:
+  print(" none of the numbers is greater than 0")
+ 
+
