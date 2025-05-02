d00e5c0e0acfd121c01b3edff079d032b1756cfa
Meher865 <jasmeetsw@gmail.com>
2025-05-02 12:47:33 +0530

    Add files via upload

---
+++ identity operator.py
@@ -0,0 +1,16 @@
+x=345
+if (type(x) is int):
+ print("true")
+else:
+ print("false")
+x= 6.0987
+if (type(x) is float):
+ print("true")
+else:
+ print("false")
+x=13
+y=45
+if x is y:
+  print("true")
+else:
+ print("false")
