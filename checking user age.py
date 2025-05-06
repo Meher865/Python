5b3a0ac3d6abf1429a8eb8c2cdf6ac6a73ea8b46
Meher865 <jasmeetsw@gmail.com>
2025-05-06 16:34:36 +0530

    Add files via upload

---
+++ checking user age.py
@@ -0,0 +1,11 @@
+age= int(input("please enter your age in years:"))
+if (age<10 ):
+    print("the user is less than 10 years old")
+elif (age >20 ):
+     print("the user is above 20 years of age")
+else:
+     if ( age <20 and age >10):
+          print ("the user is between the ages of 10 and 20")
+     else:
+          print ("the user is not between the ages of 10 and 20 years old")
+
